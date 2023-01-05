5d09200883b06a9898e3e53b3301b9bcc8c219af
Boakye2 <99751184+Boakye2@users.noreply.github.com>
2023-01-05 18:08:44 +0100

    Write a type-annotated function sum_list
    which takes a list input_list of floats as argument and returns their sum as a float.

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Create sum_list - list of floats """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Takes a list input_list of floats as argument
+    returns their sum as a float.
+    """
+
+    return sum(input_list)
+  
